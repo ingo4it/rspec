ddd536de5c9988b36dca80622ba77bde6f370d48
root <ingoklein0508@gmail.com>
2015-05-28 08:31:38 +0200

    email rspec

---
+++ mailers/user_mailer_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe ValuationLeadMailer do 
+
+  let(:lead) { ValuationLead.new({  
+      
+      full_name: 'Chris Joannou', 
+      email: 'chris@test.com',
+      phone_number: '1112223333',
+      company_name: 'Airbnb ',
+      
+    }) }
+
+  let(:mail) { UserMailer.to_user(lead) }
+  
+  describe "#to_user" do
+    it "should send the lead to the email" do
+      mail.subject.should match("Your Request for Offers")
+      mail.to.should eq(['chris@test.com'])
+      mail.from.should eq(['customersupport@test.com'])
+      mail.body.encoded.should match("Thank you for using")
+    end
+  end
+
+end
